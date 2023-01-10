f85282d6991ffb3268c6630167d67a61fe03531a
Guilherme Gama <guilhermegamabs@gmail.com>
2023-01-09 21:21:38 -0300

    Stateful

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+ state = {
+      name: 'Guilherme Gama',
+      counter: 0,
+  };
+
+  
+
+  handlePClick = () => {
+    this.setState({ name: 'Carlos'});
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({counter: counter + 1});
+  }
+
+  render(){
+    const { name, counter } = this.state;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={this.handlePClick}>
+          {name} {counter}
+        </p>
+        <a
+          onClick={this.handleAClick}
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          This is the link
+        </a>
+      </header>
+    </div>
+  );
+  }
+}
+
+export default App;
